571832944bf86a93c2417e9a344bbb50f3b82ad7
Nahshon Unna-Tsameret <nunnatsa@redhat.com>
2024-07-12 12:31:33 +0300

    range over functions

--- 00_loops_test.go
+++ 00_loops_test.go
@@ -1,6 +1,8 @@
 package playing
 
 import (
+	"fmt"
+	"math/rand/v2"
 	"testing"
 	"time"
 )
@@ -12,21 +14,21 @@ func TestFors(t *testing.T) {
 		}
 	})
 
-	t.Run("loop over slice/array", func(t *testing.T) {
+	t.Run("range over slice/array", func(t *testing.T) {
 		s := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 		for i, v := range s {
 			t.Log(i, v)
 		}
 	})
 
-	t.Run("loop over map", func(t *testing.T) {
+	t.Run("range over map", func(t *testing.T) {
 		m := map[int]string{1: "a", 2: "b", 3: "c"}
 		for k, v := range m {
 			t.Logf("%d -> %s", k, v)
 		}
 	})
 
-	t.Run("loop over channel", func(t *testing.T) {
+	t.Run("range over channel", func(t *testing.T) {
 		ch := make(chan int)
 		go func() {
 			for v := range ch {
@@ -41,14 +43,35 @@ func TestFors(t *testing.T) {
 		close(ch)
 	})
 
-	// new in go 1.22
-	t.Run("loop over int", func(t *testing.T) {
+	// new in go 1.22 - range over
+	t.Run("range over int", func(t *testing.T) {
 		for i := range 10 {
 			t.Log(i)
 		}
 
 		for range 10 {
 			t.Log("just a text")
+		}
+	})
+
+	// experimental in go 1.22 - range over function
+	// range over functions with the following signatures:
+	// func(func () bool)
+	// func(func (V) bool)
+	// func(func (K, V) bool)
+	t.Run("range over function", func(t *testing.T) {
+
+		randNumGen := func(yield func(val int) bool) {
+			loopSize := rand.N(20)
+			for range loopSize {
+				if !yield(rand.N(100)) {
+					return
+				}
+			}
+		}
+
+		for i := range randNumGen {
+			fmt.Println(i)
 		}
 	})
 }
